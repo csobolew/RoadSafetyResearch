ac7e9f9143dc211eeb0d489f7a2fb6ebfbdf75ae
mochamymocha <47485699+mochamymocha@users.noreply.github.com>
2023-08-19 20:34:29 -0400

    Added comments, command line argument for directory, and .txt file output

--- vision.py
+++ vision.py
@@ -1,51 +1,47 @@
-from PIL import Image
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import glob
-import random
+import sys
 
 def main():
-    directory = 'save'
-    test = glob.iglob(f'{directory}/*')
-    imgs1 = [cv2.imread(i) for i in test]
-    imgs = []
-    for i in range(0, 10):
-        imgs.append(random.choice(imgs1))
-    for i, j in enumerate(imgs):
-        k = j / 255.0
-        # print(np.median(k))
-        # print('mean' + str(np.mean(k)))
-        if np.median(k) < 0.582:
-            imgs[i] = (k, 1)
-        else:
-            imgs[i] = (k, 0)
-    for i in imgs:
-        if i[1] == 0:
-            print('uninverted')
-            if np.mean(i[0][47:62, 25:40]) < 0.53:
-                print('safe')
+    if len(sys.argv) > 1:
+        directory = str(sys.argv[1])
+        test = glob.iglob(f'{directory}/*')
+        f = open('output.txt', 'w')
+        # Read through all images in directory
+        imgs1 = [(cv2.imread(i), i) for i in test]
+        safe = 0
+        unsafe = 0
+        for i, j in enumerate(imgs1):
+            # Divide by 255 to get 0-1 range pixel values
+            k = j[0] / 255.0
+            # Check whether image is inverted
+            if np.median(k) < 0.582:
+                imgs1[i] = (k, 1, j[1])
             else:
-                print('unsafe')
-        else:
-            print('inverted')
-            if np.mean(i[0][47:62, 25:40]) > 0.53:
-                print('safe')
+                imgs1[i] = (k, 0, j[1])
+        for i in imgs1:
+            if i[1] == 0:
+                # If not inverted, safe if mean of area around car is less than 0.53
+                if np.mean(i[0][47:62, 25:40]) < 0.53:
+                    f.write(i[2] + ' Safe' + '\n')
+                    safe += 1
+                else:
+                    f.write(i[2] + ' Unsafe' + '\n')
+                    unsafe += 1
             else:
-                print('unsafe')
-        print('')
-    f, axarr = plt.subplots(2, 5)
-    axarr[0, 0].imshow(imgs[0][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[0, 1].imshow(imgs[1][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[0, 2].imshow(imgs[2][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[0, 3].imshow(imgs[3][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[0, 4].imshow(imgs[4][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[1, 0].imshow(imgs[5][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[1, 1].imshow(imgs[6][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[1, 2].imshow(imgs[7][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[1, 3].imshow(imgs[8][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    axarr[1, 4].imshow(imgs[9][0][47:62, 25:40], cmap='gray', vmin=0, vmax=1)
-    plt.pause(1)
+                # If inverted, safe if mean of area around car is greater than 0.53
+                if np.mean(i[0][47:62, 25:40]) > 0.53:
+                    f.write(i[2] + ' Safe' + '\n')
+                    safe += 1
+                else:
+                    f.write(i[2] + ' Unsafe' + '\n')
+                    unsafe += 1
+        f.write('Total safe: ' + str(safe) + '\n')
+        f.write('Total unsafe: ' + str(unsafe))
+        f.close()
+    else:
+        print('No image directory provided.')
 
 
 if __name__ == '__main__':
